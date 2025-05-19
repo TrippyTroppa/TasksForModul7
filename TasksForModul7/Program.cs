7d767f64bfb60198ab5db55b91cee52c973cbc5d
Egor <egork2205@gmail.com>
2025-05-19 17:27:57 +0700

    Задание 7.1.4

--- TasksForModul7/Program.cs
+++ TasksForModul7/Program.cs
@@ -11,5 +11,22 @@ namespace TasksForModul7
         static void Main(string[] args)
         {
         }
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+        class ProjectManager : Employee
+        { 
+
+         public string ProjectName;
+
+        }
+
+        class Developer : Employee
+        {
+         public string ProgrammingLanguage;
+        }
     }
 }
