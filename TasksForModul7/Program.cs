fcccd1df984eacf9157d8aea6bb1712172e28757
Egor <egork2205@gmail.com>
2025-06-05 13:05:08 +0700

    Задание 7.5.5

--- TasksForModul7/Program.cs
+++ TasksForModul7/Program.cs
@@ -13,22 +13,25 @@ namespace TasksForModul7
     {
         static void Main(string[] args)
         {
-            int num1 = 58;
-            int num2 = 3;
-
-            Helper.Swap(ref num1, ref num2);
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
+           
         }
-       class Helper
+        class Obj
         {
-         public static void Swap (ref int a, ref int b)
+            public string Name;
+            public string Description;
+
+            public static string Parent;
+            public static int DaysInWeek;
+            public static int MaxValue;
+
+            static Obj()
             {
-                int c = a;
-                a = b;
-                b = c;
+                Parent = "System.Object";
+                DaysInWeek = 7;
+                MaxValue = 2000;
+
             }
         }
-        
+
     }
 }
