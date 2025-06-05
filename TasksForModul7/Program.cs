539d4059303e853c3150aff082488df5524f56b6
Egor <egork2205@gmail.com>
2025-06-05 13:37:46 +0700

    Задание 7.6.2

--- TasksForModul7/Program.cs
+++ TasksForModul7/Program.cs
@@ -9,34 +9,23 @@ using TasksForModul7;
 
 namespace TasksForModul7
 {
-   
-    public static class IntExtensions
-    {
-        public static int GetNegative(this int number)
-        {
-            return number > 0 ? -number : number;
-        }
-
-        public static int GetPositive(this int number)
-        {
-            return number < 0 ? -number : number;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
 
-            Console.WriteLine(num1.GetNegative()); // -7
-            Console.WriteLine(num1.GetPositive()); // 7
-            Console.WriteLine(num2.GetNegative()); // -13
-            Console.WriteLine(num2.GetPositive()); // 13
-            Console.WriteLine(num3.GetNegative()); // 0
-            Console.WriteLine(num3.GetPositive()); // 0
         }
     }
+
+
+    class ElectricEngine { }
+
+    class GasEngine { }
+
+    class Car<T>
+    {
+        public T Engine;
+    }
+
+    
 }
